cc4de5bdce8e6f4dbcba286745f14217530d5cb4
juniorsynctech <56076472+juniorsynctech@users.noreply.github.com>
2020-04-03 12:16:06 -0300

    Add files via upload

---
+++ src/main/java/com/apicongrega/apicongrega/ApiCongregaApplication.java
@@ -0,0 +1,13 @@
+package com.apicongrega.apicongrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCongregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCongregaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apicongrega/apicongrega/ApiCongregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicongrega.apicongrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCongregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
